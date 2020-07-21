9fd17fb7e6e5623e2433ca7a38e3bb5218a1c264
Omer Menashe <46810647+iamomerm@users.noreply.github.com>
2020-07-21 06:04:35 +0300

    July 21, 2020

---
+++ IP-Scanner/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace IPScanner
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.RTB = new System.Windows.Forms.RichTextBox();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.ActionBtn = new System.Windows.Forms.Button();
+            this.FromLabel = new System.Windows.Forms.Label();
+            this.ToLabel = new System.Windows.Forms.Label();
+            this.SourceText = new System.Windows.Forms.MaskedTextBox();
+            this.DestText = new System.Windows.Forms.MaskedTextBox();
+            this.RangeLabel = new System.Windows.Forms.Label();
+            this.TimeOutLabel = new System.Windows.Forms.Label();
+            this.TimeOutText = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // RTB
+            // 
+            this.RTB.BackColor = System.Drawing.Color.White;
+            this.RTB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.RTB.Location = new System.Drawing.Point(18, 87);
+            this.RTB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.RTB.Name = "RTB";
+            this.RTB.Size = new System.Drawing.Size(314, 238);
+            this.RTB.TabIndex = 7;
+            this.RTB.Text = "";
+            this.RTB.TextChanged += new System.EventHandler(this.RTBTextChange);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ExitBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ExitBtn.FlatAppearance.BorderSize = 0;
+            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitBtn.Location = new System.Drawing.Point(227, 342);
+            this.ExitBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(105, 33);
+            this.ExitBtn.TabIndex = 6;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtnClick);
+            // 
+            // ActionBtn
+            // 
+            this.ActionBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(255)))));
+            this.ActionBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ActionBtn.FlatAppearance.BorderSize = 0;
+            this.ActionBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ActionBtn.ForeColor = System.Drawing.Color.White;
+            this.ActionBtn.Location = new System.Drawing.Point(116, 342);
+            this.ActionBtn.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ActionBtn.Name = "ActionBtn";
+            this.ActionBtn.Size = new System.Drawing.Size(105, 33);
+            this.ActionBtn.TabIndex = 5;
+            this.ActionBtn.Text = "Scan";
+            this.ActionBtn.UseVisualStyleBackColor = false;
+            this.ActionBtn.Click += new System.EventHandler(this.ActionBtnClick);
+            // 
+            // FromLabel
+            // 
+            this.FromLabel.AutoSize = true;
+            this.FromLabel.Location = new System.Drawing.Point(15, 35);
+            this.FromLabel.Name = "FromLabel";
+            this.FromLabel.Size = new System.Drawing.Size(35, 15);
+            this.FromLabel.TabIndex = 4;
+            this.FromLabel.Text = "From";
+            // 
+            // ToLabel
+            // 
+            this.ToLabel.AutoSize = true;
+            this.ToLabel.Location = new System.Drawing.Point(199, 36);
+            this.ToLabel.Name = "ToLabel";
+            this.ToLabel.Size = new System.Drawing.Size(19, 15);
+            this.ToLabel.TabIndex = 2;
+            this.ToLabel.Text = "To";
+            // 
+            // SourceText
+            // 
+            this.SourceText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.SourceText.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.SourceText.Location = new System.Drawing.Point(75, 36);
+            this.SourceText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.SourceText.Mask = "000.000.000.000";
+            this.SourceText.Name = "SourceText";
+            this.SourceText.PromptChar = '#';
+            this.SourceText.Size = new System.Drawing.Size(104, 16);
+            this.SourceText.TabIndex = 1;
+            this.SourceText.Text = "192168100100";
+            this.SourceText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DestText
+            // 
+            this.DestText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DestText.Location = new System.Drawing.Point(227, 36);
+            this.DestText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DestText.Mask = "000.000.000.000";
+            this.DestText.Name = "DestText";
+            this.DestText.PromptChar = '#';
+            this.DestText.Size = new System.Drawing.Size(105, 16);
+            this.DestText.TabIndex = 0;
+            this.DestText.Text = "192168100200";
+            this.DestText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // RangeLabel
+            // 
+            this.RangeLabel.AutoSize = true;
+            this.RangeLabel.Location = new System.Drawing.Point(15, 8);
+            this.RangeLabel.Name = "RangeLabel";
+            this.RangeLabel.Size = new System.Drawing.Size(102, 15);
+            this.RangeLabel.TabIndex = 3;
+            this.RangeLabel.Text = "IP-Scanner Range";
+            // 
+            // TimeOutLabel
+            // 
+            this.TimeOutLabel.AutoSize = true;
+            this.TimeOutLabel.Location = new System.Drawing.Point(15, 62);
+            this.TimeOutLabel.Name = "TimeOutLabel";
+            this.TimeOutLabel.Size = new System.Drawing.Size(51, 15);
+            this.TimeOutLabel.TabIndex = 9;
+            this.TimeOutLabel.Text = "Timeout";
+            // 
+            // TimeOutText
+            // 
+            this.TimeOutText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TimeOutText.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.TimeOutText.Location = new System.Drawing.Point(75, 63);
+            this.TimeOutText.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TimeOutText.Mask = "0000";
+            this.TimeOutText.Name = "TimeOutText";
+            this.TimeOutText.PromptChar = '#';
+            this.TimeOutText.Size = new System.Drawing.Size(104, 16);
+            this.TimeOutText.TabIndex = 8;
+            this.TimeOutText.Text = "1000";
+            this.TimeOutText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(246)))), ((int)(((byte)(246)))), ((int)(((byte)(246)))));
+            this.ClientSize = new System.Drawing.Size(349, 390);
+            this.Controls.Add(this.TimeOutText);
+            this.Controls.Add(this.TimeOutLabel);
+            this.Controls.Add(this.DestText);
+            this.Controls.Add(this.SourceText);
+            this.Controls.Add(this.ToLabel);
+            this.Controls.Add(this.RangeLabel);
+            this.Controls.Add(this.FromLabel);
+            this.Controls.Add(this.ActionBtn);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.RTB);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "IPv4 Scanner";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox RTB;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Button ActionBtn;
+        private System.Windows.Forms.Label FromLabel;
+        private System.Windows.Forms.Label ToLabel;
+        private System.Windows.Forms.MaskedTextBox SourceText;
+        private System.Windows.Forms.MaskedTextBox DestText;
+        private System.Windows.Forms.Label RangeLabel;
+        private System.Windows.Forms.Label TimeOutLabel;
+        private System.Windows.Forms.MaskedTextBox TimeOutText;
+    }
+}
+
---
+++ IP-Scanner/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+namespace IPScanner
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //Threads
+        Thread ScanThread; /* 1 */
+
+        //IPv4 Octet Fix Format Method
+        private string IPFixFormat(string Octet)
+        {
+            for (int Index = 0; Index <= Octet.Length; Index++)
+            {
+                if (Octet[0] == '0') { Octet = Octet.Substring(1, Octet.Length - 1); /* Trim String */ }
+
+                else { break; } /* Stop When Other Digit Appears */      
+            }
+
+            return Octet;
+        }
+
+        //IPv4 Address Validation (IPAdress.TryParse is Not Working Properly)
+        private bool IPValidation(string IPAddress)
+        {
+            string[] IPAddressSplit = IPAddress.Split('.');
+
+            if (IPAddressSplit.Length != 4) { return false; } /* Return False */
+
+            else
+            {
+                for (int Index = 0; Index < 4; Index++)
+                {
+                    int Octet = 0;
+                        
+                    bool IntValidation = Int32.TryParse(IPAddressSplit[Index], out Octet);
+
+                    if (IntValidation == false) { return false; } /* Return False */
+
+                    if (Octet > 255) { return false; } /* Return False */
+
+                    if ((Index == 0) &&(Octet == 0)) { return false; } /* Return False - 1st Octet Validation */
+                }
+            }
+
+            return true; /* Return True */
+        }
+
+        //Text Colors Enums
+        enum TextColors { Black, Green, Red };
+
+        //Rich Text Box AppendText
+        private void RTBAppendText(string Text, TextColors Color)
+        {
+            switch (Color)
+            {
+                case TextColors.Black:
+                    RTB.Invoke(new MethodInvoker(delegate { RTB.SelectionColor = System.Drawing.Color.Black; RTB.AppendText(Text); }));
+                    break;
+
+                case TextColors.Green:
+                    RTB.Invoke(new MethodInvoker(delegate { RTB.SelectionColor = System.Drawing.Color.Green; RTB.AppendText(Text); }));
+                    break;
+
+                case TextColors.Red:
+                    RTB.Invoke(new MethodInvoker(delegate { RTB.SelectionColor = System.Drawing.Color.Red; RTB.AppendText(Text); }));
+                    break;
+
+                default:
+                    break;
+            }                  
+        }
+
+        //Rich Text Box Clear
+        private void RTBClear()
+        {
+            RTB.Invoke(new MethodInvoker(delegate { RTB.Clear(); }));
+        }
+
+        //Rich Text Box Refresh
+        private void RTBRefresh()
+        {
+            RTB.Invoke(new MethodInvoker(delegate { RTB.Refresh(); }));
+        }
+
+        //Action Button Swich Action (Scan\Stop)
+        int Action = 0; /* Default = Scan */
+
+        private void ActionButtonSwich()
+        {
+            if (Action == 0) /* Scan */
+            {
+                ActionBtn.Invoke(new MethodInvoker(delegate 
+                { 
+                    ActionBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(50)))), ((int)(((byte)(255)))));
+
+                    ActionBtn.Text = "Stop";
+                }));
+
+                Action = 1; /* Switch Action */
+            }
+
+            else if (Action == 1) /* Stop */
+            {
+                ActionBtn.Invoke(new MethodInvoker(delegate
+                {
+                    ActionBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(255)))));
+
+                    ActionBtn.Text = "Scan";
+                }));
+
+                Action = 0; /* Switch Action */
+            }
+        }
+
+        //Scan Method
+        public void Scan()
+        {
+            ActionButtonSwich(); /* Scan */
+
+            RTBClear(); 
+
+            //Source Address
+            string SourceAddress = this.SourceText.Text;
+
+            //Time Out
+            int TimeOut = 0;
+
+            Int32.TryParse(this.TimeOutText.Text, out TimeOut);
+
+            if (this.TimeOutText.Text.Contains(' '))
+            {
+                RTBAppendText("[Error] Invalid Timeout Expression!" + "\n", TextColors.Black);
+
+                TimeOut = 0; /* Set to Default Timeout */
+            }
+
+            if (TimeOut == 0)
+            {
+                TimeOut = 1000;
+
+                RTBAppendText("Timeout Set to 1000ms (Default)" + " \n", TextColors.Black);
+
+                RTBRefresh();
+            }
+
+            else { RTBAppendText("Timeout Set to " + TimeOut.ToString() + "ms" + "\n", TextColors.Black); }
+
+            //Destination Address
+            string DestAddress = this.DestText.Text;
+
+            //# Validation #
+            bool SourceValidation = IPValidation(SourceAddress);
+
+            bool DestValidation = IPValidation(DestAddress);
+
+            if ((SourceValidation == false) || (DestValidation == false))
+            {
+                RTBAppendText("Source Address: " + SourceAddress + "\n", TextColors.Black);
+
+                RTBAppendText("Destination Address: " + DestAddress + "\n", TextColors.Black);
+
+                RTBAppendText("[Error] Invalid Source or Destination Addresses!" + "\n", TextColors.Black);
+
+                RTBRefresh();
+            }
+
+            else
+            {
+                long SourceLong = Convert.ToInt64(SourceAddress.Replace(".", ""));
+
+                long DestLong = Convert.ToInt64(DestAddress.Replace(".", ""));
+
+                if (SourceLong >= DestLong)
+                {
+                    RTBAppendText("Source Address: " + SourceAddress + "\n", TextColors.Black);
+
+                    RTBAppendText("Destination Address: " + DestAddress + "\n", TextColors.Black);
+
+                    RTBAppendText("[Error] Invalid IP Addresses Range!" + "\n", TextColors.Black);
+
+                    RTBRefresh();
+                }
+
+                else
+                {
+                    //Fix IP Address Format
+
+                    //Source
+                    string[] SourceSplit = SourceAddress.Split('.');
+                    string SourceOctetOne = IPFixFormat(SourceSplit[0]);
+                    string SourceOctetTwo = IPFixFormat(SourceSplit[1]);
+                    string SourceOctetThree = IPFixFormat(SourceSplit[2]);
+                    string SourceOctetFour = IPFixFormat(SourceSplit[3]);
+                    SourceAddress = SourceOctetOne + "." + SourceOctetTwo + "." + SourceOctetThree + "." + SourceOctetFour; /* Modify Source Address */
+                    System.Net.IPAddress SourceIP = System.Net.IPAddress.Parse(SourceAddress); /* Source IP */
+
+                    //Destination
+                    string[] DestSplit = DestAddress.Split('.');
+                    string DestOctetOne = IPFixFormat(DestSplit[0]);
+                    string DestOctetTwo = IPFixFormat(DestSplit[1]);
+                    string DestOctetThree = IPFixFormat(DestSplit[2]);
+                    string DestOctetFour = IPFixFormat(DestSplit[3]);
+                    DestAddress = DestOctetOne + "." + DestOctetTwo + "." + DestOctetThree + "." + DestOctetFour; /* Modify Destination Address */
+                    System.Net.IPAddress DestIP = System.Net.IPAddress.Parse(DestAddress); /* Destination IP */
+
+                    Byte[] SourceBytes = SourceIP.GetAddressBytes();
+
+                    while (true)
+                    {
+                        SourceBytes[3] = (byte)(SourceBytes[3] + 1); /* 4th Octet */
+
+                        if (SourceBytes[3] == 0)
+                        {
+                            SourceBytes[2] = (byte)(SourceBytes[2] + 1); /* 3rd Octet */
+
+                            if (SourceBytes[2] == 0)
+                            {
+                                SourceBytes[1] = (byte)(SourceBytes[1] + 1); /* 2nd Octet */
+
+                                if (SourceBytes[1] == 0)
+                                {
+                                    SourceBytes[0] = (byte)(SourceBytes[0] + 1); /* 1st Octet */
+                                }
+                            }
+                        }
+
+                        string CurrentAddress = SourceBytes[0].ToString() + "." + SourceBytes[1].ToString() + "." + SourceBytes[2].ToString() + "." + SourceBytes[3].ToString();
+
+                        //Test Communication
+                        Ping Pinger = new Ping();
+
+                        PingReply Reply = Pinger.Send(CurrentAddress, TimeOut);
+
+                        if (Reply.Status == IPStatus.Success)
+                        {
+                            RTBAppendText("[" + CurrentAddress + "]" + " Connection Established!" + "\n", TextColors.Green);
+                        }
+
+                        else
+                        {
+                            RTBAppendText("[" + CurrentAddress + "]" + " Device is Unavailable!" + "\n", TextColors.Red);
+                        }
+
+                        Pinger.Dispose();
+
+                        RTBRefresh();
+
+                        if (CurrentAddress == DestAddress) { break; /* Break Condition */ }                        
+                    }
+                }
+            }
+
+            RTBAppendText("Finished!" + "\n", TextColors.Black);
+
+            ActionButtonSwich(); /* Stop */
+        }
+
+        private void ActionBtnClick(object sender, EventArgs e)
+        {
+            if (Action == 0) /* Scan */
+            {
+                ScanThread = new Thread(new ThreadStart(Scan));
+
+                ScanThread.IsBackground = true;
+
+                ScanThread.Start();
+            } 
+
+            else if (Action == 1) /* Stop */
+            {
+                ActionButtonSwich();
+
+                ScanThread.Suspend();
+
+                ScanThread.Abort();
+
+                RTBAppendText("Stopped!" + "\n", TextColors.Black);
+            } 
+        }
+
+        private void ExitBtnClick(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.ExitThread(); /* Exit Application */
+        }
+
+        private void RTBTextChange(object sender, EventArgs e)
+        {
+            RTB.SelectionStart = RTB.Text.Length;
+
+            RTB.ScrollToCaret();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //Check Multiple Instances
+            bool SingleInstance;
+
+            Mutex Mutex = new Mutex(true, "Application", out SingleInstance);
+
+            if (!SingleInstance)
+            {
+                MessageBox.Show("Instance of 'IPv4 Scanner' Is Already Running !", "IPv4 Scanner");
+
+                //Exit Application
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+    }
+}
